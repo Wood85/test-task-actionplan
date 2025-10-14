15c83503a6be9d76bc89d99094a10052e7021bf9
Aleksandr Krivoshein <megalistnn@gmail.com>
2025-10-14 22:27:26 +0400

    chore: project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Task Actionplay
+
+This project is a component for displaying and filtering project participants using TanStack Query.
---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+const aliasList = [
+  'app',
+  'assets',
+  'components',
+  'config',
+  'lib',
+  'styles',
+  'services',
+  'app-types',
+  'utils',
+  'app'
+];
+
+const alias = aliasList.reduce(
+  (acc, name) => {
+    const dir = name === 'app-types' ? 'src/types' : `src/${name}`;
+    acc[`@${name}`] = path.resolve(__dirname, dir);
+    return acc;
+  },
+  {} as Record<string, string>
+);
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    fs: {}
+  },
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']]
+      }
+    })
+  ],
+  resolve: {
+    alias
+  },
+  css: {
+    devSourcemap: true
+  },
+  build: {
+    rollupOptions: {
+      treeshake: true
+    }
+  }
+});
